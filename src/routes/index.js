d7d4a1678c517ee699bc3b0feb05d8f80fc2c8ab
Franco rossi <rossifranco748@gmail.com>
2023-08-06 21:45:34 -0300

    Arreglando rutas de usuario.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import inmuebles from './inmueblesRoutes'
+import admins from './userRoutes'
 const router = express.Router();
 
 router.use('/inmuebles', inmuebles);
+router.use('/user', admins);
 export default router;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,27 +1,10 @@
-const express = require("express");
+import express from "express";
+import { login, register } from "../controllers/userController.js";
 const router = express.Router();
-const { listUser, login, register, infoUser, deleteUser } = require("../controllers/userController");
-const { runValidation } = require("../validators/index");
 
-const {
-  userValidator,
-  loginValidator,
-} = require("../validators/userValidator.js");
+// router.get("/", listUser);
+ router.post("/login",  login);
+router.post("/", register);
+//router.delete("/:id", verifyToken, verifyPerfil, deleteUser);
 
-const { verifyToken } = require("../validators/auth");
-const { verifyPerfil} = require("../validators/perfil");
-
-
-router.get("/user/infoUser", verifyToken, infoUser)
-
-router.get("/user/list",  listUser);
-
-router.post("/user/login",  loginValidator, runValidation, login);
-
-router.post("/user/register",  register);
-
-router.delete("/user/deleteUser/:id", verifyToken, verifyPerfil,  deleteUser);
-
-module.exports = router;
-
-
+export default  router;
