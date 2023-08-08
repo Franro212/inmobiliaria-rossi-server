92bf50c9b3884c0269788f6ead4bab29dee37247
Franco rossi <rossifranco748@gmail.com>
2023-08-08 18:35:38 -0300

    Arreglo de rutas.

--- src/routes/inmueblesRoutes.js
+++ src/routes/inmueblesRoutes.js
@@ -1,20 +1,32 @@
 import express from "express";
-import { createInmueble, deleteInmueble, getInmuebleByID, getInmuebles, updateInmueble } from '../controllers/inmuebleController'
+import {
+  createInmueble,
+  deleteInmueble,
+  getInmuebleByID,
+  getInmuebles,
+  updateInmueble,
+} from "../controllers/inmuebleController";
 import multer from "multer";
 import verifyToken from "../validators/auth";
 import { verifyPerfil } from "../validators/perfil";
 import validateInmuebles from "../validators/inmueblesValidation";
-
 
 const router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 router
-  .get("/", verifyToken,  getInmuebles)
+  .get("/", getInmuebles)
   .get("/:id", verifyToken, getInmuebleByID)
-  .post("/", verifyToken,verifyPerfil,validateInmuebles, upload.single("images"), createInmueble)
-  .put("/:id", verifyToken,verifyPerfil,validateInmuebles, updateInmueble)
+  .post(
+    "/",
+    verifyToken,
+    verifyPerfil,
+    validateInmuebles,
+    upload.single("images"),
+    createInmueble,
+  )
+  .put("/:id", verifyToken, verifyPerfil, validateInmuebles, updateInmueble)
   .delete("/:id", verifyToken, verifyPerfil, deleteInmueble);
 
 export default router;
